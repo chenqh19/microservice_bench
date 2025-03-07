0b57379dabdf1b0a1738f072121a0b03e1d6d32a
chenqh19 <chenqh23@mit.edu>
2025-03-07 20:02:34 +0000

    100 timeout

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,12 +1,20 @@
 import time
 from locust import FastHttpUser, task, between, constant_throughput
 
-class QuickstartUser(FastHttpUser):
+class HotelUser(FastHttpUser):
     wait_time = constant_throughput(20)
 
     run_time = "30s"
 
-    @task
+    # You can adjust these weights to change the proportion of different requests
+    TASK_WEIGHTS = {
+        "search": 40,
+        "recommend": 30,
+        "user": 15,
+        "reservation": 15
+    }
+
+    @task(TASK_WEIGHTS["search"])
     def search_hotels(self):
         query_params = {
             "customerName": "John",
@@ -17,3 +25,34 @@ class QuickstartUser(FastHttpUser):
             "locale": "en"
         }
         self.client.get("/search", params=query_params, name="search_hotels")
+
+    @task(TASK_WEIGHTS["recommend"])
+    def get_recommendations(self):
+        query_params = {
+            "latitude": "37.7749",
+            "longitude": "-122.4194",
+            "require": "near_city",
+            "locale": "en"
+        }
+        self.client.get("/recommend", params=query_params, name="get_recommendations")
+
+    @task(TASK_WEIGHTS["user"])
+    def user_login(self):
+        query_params = {
+            "username": "Cornell_1",
+            "password": "1111111111"
+        }
+        self.client.get("/user", params=query_params, name="user_login")
+
+    @task(TASK_WEIGHTS["reservation"])
+    def make_reservation(self):
+        query_params = {
+            "customerName": "John",
+            "hotelId": "1",
+            "inDate": "2023-12-01",
+            "outDate": "2023-12-02",
+            "roomNumber": "1",
+            "username": "Cornell_1",
+            "password": "1111111111"
+        }
+        self.client.get("/reservation", params=query_params, name="make_reservation")
