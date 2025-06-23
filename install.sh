bc6c712a329417a5fd6b18f878d1fdd7ff5a9d26
czarkos <chrisarkos16@gmail.com>
2025-06-23 16:47:28 -0700

    Changes that allow docker to have access to the IAA hardware

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 
 # build images
-sudo docker compose build
+#sudo docker compose build
 
 # install wrk2
 yes | sudo apt-get install luarocks
 yes | sudo luarocks install luasocket
 yes | sudo luarocks install dkjson
-cd ../examples/wrk2
+cd wrk2
 make clean
 make
 
 
-./wrk2/wrk -D exp -t 100 -c 100 -d 50 -L -s ./wrk_scripts/scripts/hotel-reservation/mixed-workload_type_1.lua http://localhost:50050 -R 2000
+./wrk -D exp -t 100 -c 100 -d 50 -L -s ../wrk_scripts/scripts/hotel-reservation/mixed-workload_type_1.lua http://localhost:50050 -R 2000
