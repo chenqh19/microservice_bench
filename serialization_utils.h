4fcdc51cb2ca21dda0d3ff489899ca5eabe4d6ee
czarkos <chrisarkos16@gmail.com>
2025-07-14 14:11:24 -0700

    Comment out debug prints

--- serialization_utils.h
+++ serialization_utils.h
@@ -20,25 +20,25 @@ static Ser1de_re& get_ser1de_instance() {
 // Serialize protobuf message to string using ser1de
 template<typename T>
 std::string serialize_message(const T& message) {
-    std::cout << "Using SERenaDE for serialization" << std::endl;
+    //std::cout << "Using SERenaDE for serialization" << std::endl;
     std::string serialized;
     // Cast to base Message type for ser1de
     T& non_const_message = const_cast<T&>(message);
     get_ser1de_instance().SerializeToString(non_const_message, &serialized);
-    std::cout << "Used SERenaDE for serialization successfully!" << std::endl;
+    //std::cout << "Used SERenaDE for serialization successfully!" << std::endl;
     return serialized;
 }
 
 // Deserialize string to protobuf message using ser1de
 template<typename T>
 bool deserialize_message(const std::string& data, T& message) {
-    std::cout << "Using SERenaDE for deserialization" << std::endl;
+    //std::cout << "Using SERenaDE for deserialization" << std::endl;
     try {
         get_ser1de_instance().ParseFromString(data, &message);
-        std::cout << "Used SERenaDE for deserialization successfully!" << std::endl;
+        //std::cout << "Used SERenaDE for deserialization successfully!" << std::endl;
         return true;
     } catch (const std::exception& e) {
-        std::cout << "Failed to deserialize message using SERenaDE" << std::endl;
+        //std::cout << "Failed to deserialize message using SERenaDE" << std::endl;
         return false;
     }
 }
